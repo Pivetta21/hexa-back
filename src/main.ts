faee7feac8b2ef2d7fd42e07aa9dc5943adfd98c
Lucas Pivetta <lucaspivetta21@gmail.com>
2021-07-04 19:51:49 -0300

    chore: add OpenAPI and fix dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Hexa API')
+    .setDescription('API utilizada na versão web e mobile.')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(8080);
 }
 bootstrap();
